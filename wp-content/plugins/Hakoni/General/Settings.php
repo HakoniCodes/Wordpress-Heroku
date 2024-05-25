adcb562ea1df449cfa87efc96e89fe007db6a5a5
Makana Edwards <makrigamedeveloping@gmail.com>
2024-05-25 16:14:13 -0400

    backing up

--- wp-content/plugins/Hakoni/General/Settings.php
+++ wp-content/plugins/Hakoni/General/Settings.php
@@ -19,19 +19,12 @@ class Settings extends Base {
 
 	function registerMenus() {
 		add_menu_page('Hakoni', 'Hakoni', 'manage_options', 'hakoni_settings_page', [$this, 'registerPage'], 'dashicons-admin-tools', 1);
-		// add_submenu_page('hakoni_settings_page', 'Authentication', 'Authentication', 'manage_options', 'hakoni_settings_authentication_page', [$this, 'registerAuthenticationPage'], 1);
 	}
 
 
 	function registerPage() {
 		?>
 			<h1>Hakoni Settings Page</h1>
-		<?php
-	}
-
-	function registerAuthenticationPage() {
-		?>
-			<h1>Authentication Settings Page</h1>
 		<?php
 	}
 }
