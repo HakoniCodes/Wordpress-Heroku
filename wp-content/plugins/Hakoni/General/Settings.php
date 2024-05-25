9e17603d6ee375860c6876886095a2e91e248ac2
Makana Edwards <makrigamedeveloping@gmail.com>
2024-05-25 16:40:24 -0400

    backing up

--- wp-content/plugins/Hakoni/General/Settings.php
+++ wp-content/plugins/Hakoni/General/Settings.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 
 namespace Hakoni\General;
 
-add_action('admin_menu', [$this, 'registerMenus']);
+add_action('admin_menu', 'registerMenus');
 	
 function registerMenus() {
 	add_menu_page('Hakoni', 'Hakoni', 'manage_options', 'hakoni_settings_page', 'registerPage', 'dashicons-admin-tools', 1);
